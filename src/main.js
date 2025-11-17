b90ec68fbe0b73032a9bf0ea93cb4533ce279c93
girassoisazuis2308-ops <girassoisazuis2308@gmail.com>
2025-11-17 12:30:44 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -234,7 +234,7 @@ limparMonstros() {
         <div class="stats-row">
           <div class="stat-box" style="text-align:center;">
             <label class="label" style="margin-bottom:6px; display:block;">Função</label>
-            <select v-model="Tipo" style="width:100%;text-align:center;">
+            <select v-model="tipo" style="width:100%;text-align:center;">
               <option>Combatente</option>
               <option>Arruinado</option>
             </select>
